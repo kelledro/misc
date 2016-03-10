51fd6616d2c640eed199735b0c2106e807548361
Rob Kelledy <rkelledy@atlassian.com>
2016-03-10 16:23:31 +1100

    new test

--- r53proptest_new.py
+++ r53proptest_new.py
@@ -1,23 +1,36 @@
 #!/usr/bin/python
+
+# This tests the propagation time of new records added to a continually growing zone
+# It also tests for propagation by querying against authoratative name servers
+# rather than querying the R53 API and checking fon INSYNC status since this a
+# better real world test. Propagation is considered complete when all auth nameservers
+# respond to the query
+
 import boto3
 import time
 import datetime
 import pytz
+import socket
 
+zoneId = "Z3JNDBS8LPKXRY"
+authNSList = ""
+counter = 0
 client = boto3.client('route53')
 f = open('proptime.log','w')
+
 while True:
-	time.sleep(30)
+	counter += 1
+	hostname = str(counter) + 'testing.com'
 	# Make update
 	upsertResponse = client.change_resource_record_sets(
-    		HostedZoneId='Z1FKJAMHMERS1R',
+    		HostedZoneId=zoneId,
     		ChangeBatch={
-       		 	'Comment': 'testing boto3',
+       		 	'Comment': 'testing update time',
         		'Changes': [
            		 {
                			 'Action': 'UPSERT',
                			 'ResourceRecordSet': {
-                   			'Name': 'www.kelledro.com',
+                   			'Name': hostname,
                	   			'Type': 'A',
                    			'TTL': 60,
                    			'ResourceRecords': [
@@ -31,7 +44,7 @@ while True:
 		}
 	)
 
-	# Get change Status
+	# Check for record
 	getChangeResponse = client.get_change(
 		Id=upsertResponse['ChangeInfo']['Id']
 	)
@@ -47,5 +60,9 @@ while True:
 	elapsed = datetime.datetime.now(pytz.UTC) - upsertResponse['ChangeInfo']['SubmittedAt']
 	requestId = upsertResponse['ResponseMetadata']['RequestId']
 	submitted = upsertResponse['ChangeInfo']['SubmittedAt']
+
+	# write to log
 	f.write('Elapsed: ' + str(elapsed) + ' - RequestId: ' + requestId + ' - Submitted: ' + str(submitted) + '\n')
 	f.flush()
+
+	time.sleep(30)
